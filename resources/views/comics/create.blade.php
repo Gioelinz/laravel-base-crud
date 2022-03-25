0cefcf9e91a2314906e7fb5ef3986d66ff5bed3e
Gioelinz <gioelerunza.gr@gmail.com>
2022-03-25 09:14:28 +0100

    add first validating and with js title keyup

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -17,43 +17,49 @@
             @csrf
             <div class="input-group mb-5">
                 <span class="input-group-text" id="basic-addon1">Title</span>
-                <input type="text" class="form-control me-3" placeholder="Enter Title" value="{{ old('title') }}"
-                    name="title" required>
+                <input id="title" type="text" class="form-control me-3 @error('title') is-invalid @enderror"
+                    placeholder="Enter Title" value="{{ old('title') }}" name="title" required>
+                {{-- @error('title')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror --}}
                 <span class="input-group-text" id="basic-addon1">Series</span>
-                <input type="text" class="form-control" placeholder="Enter Series" name="series"
-                    value="{{ old('series') }}" required>
+                <input type="text" class="form-control @error('series') is-invalid @enderror" placeholder="Enter Series"
+                    name="series" value="{{ old('series') }}" required>
             </div>
 
             <div class="input-group mb-5">
-                <input type="date" class="form-control" placeholder="Sale Date" name="sale_date"
-                    value="{{ old('sale_date') }}" required>
+                <input type="date" class="form-control @error('sale_date') is-invalid @enderror" placeholder="Sale Date"
+                    name="sale_date" value="{{ old('sale_date') }}" required>
             </div>
 
 
             <div class="input-group mb-5">
 
                 <span class="input-group-text">https://example.com/image/</span>
-                <input type="text" id="url-prev" class="form-control" placeholder="Comic image URL" name="thumb"
-                    value="{{ old('thumb') }}" required>
+                <input type="text" id="url-prev" class="form-control @error('thumb') is-invalid @enderror"
+                    placeholder="Comic image URL" name="thumb" value="{{ old('thumb') }}" required>
 
                 <img class="img-fluid preview" src="" alt="preview">
             </div>
 
             <div class="input-group mb-5">
                 <span class="input-group-text">€</span>
-                <input type="number" class="form-control" default="0" min="0" step="0.01" placeholder="Price" name="price"
-                    value="{{ old('price') }}" required>
+                <input type="number" class="form-control @error('price') is-invalid @enderror" default="0" min="0"
+                    step="0.01" placeholder="Price" name="price" value="{{ old('price') }}" required>
                 <span class="input-group-text">.00</span>
             </div>
 
             <div class="input-group mb-5">
-                <input type="text" class="form-control" placeholder="Type" name="type" value="{{ old('type') }}"
-                    required>
+                <input type="text" class="form-control @error('type') is-invalid @enderror" placeholder="Type" name="type"
+                    value="{{ old('type') }}" required>
             </div>
 
             <div class="input-group">
                 <span class="input-group-text">Decription</span>
-                <textarea class="form-control" name="description" required>{{ old('description') }}</textarea>
+                <textarea class="form-control @error('description') is-invalid @enderror" name="description"
+                    required>{{ old('description') }}</textarea>
             </div>
             <div class="d-flex justify-content-end gap-2">
                 <button class="btn btn-danger mt-3" type="reset">Reset</button>
@@ -69,4 +75,20 @@
 
 @section('scripts')
     <script src="{{ asset('js/preview-url.js') }}"></script>
+    <script>
+        const title = document.getElementById('title');
+
+        title.onkeyup = function() {
+            const value = title.value;
+
+            console.log(value);
+            if (value.length >= 5) {
+                title.classList.add('is-valid')
+                title.classList.remove('is-invalid')
+            } else {
+                title.classList.add('is-invalid')
+                title.classList.remove('is-valid')
+            }
+        };
+    </script>
 @endsection
